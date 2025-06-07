412c382b142f13e2c17154259251222916fc7ced
MathiasKalb <mkalb@mkalb.de>
2025-06-07 21:36:57 +0200

    Optimize WaTorTerritory

--- app/src/main/java/de/mkalb/etpetssim/wator/WaTorTerritory.java
+++ app/src/main/java/de/mkalb/etpetssim/wator/WaTorTerritory.java
@@ -4,51 +4,51 @@ import org.jspecify.annotations.Nullable;
 
 import java.util.*;
 
-public final class WaTorTerritory {
+final class WaTorTerritory {
 
     private final int xSize;
     private final int ySize;
     private final @Nullable Long[] grid;
 
-    public WaTorTerritory(int xSize, int ySize) {
+    WaTorTerritory(int xSize, int ySize) {
         this.xSize = xSize;
         this.ySize = ySize;
         grid = new Long[xSize * ySize];
     }
 
-    public boolean isEmpty(WaTorCoordinate coordinate) {
+    boolean isEmpty(WaTorCoordinate coordinate) {
         return grid[coordinate.gridIndex(xSize)] == null;
     }
 
-    public boolean isNotEmpty(WaTorCoordinate coordinate) {
+    boolean isNotEmpty(WaTorCoordinate coordinate) {
         return grid[coordinate.gridIndex(xSize)] != null;
     }
 
-    public Optional<Long> findIdAt(WaTorCoordinate coordinate) {
+    Optional<Long> findIdAt(WaTorCoordinate coordinate) {
         return Optional.ofNullable(grid[coordinate.gridIndex(xSize)]);
     }
 
-    public void placeIdAt(long id, WaTorCoordinate coordinate) {
+    void placeIdAt(long id, WaTorCoordinate coordinate) {
         if (!isEmpty(coordinate)) {
-            throw new IllegalStateException("placeIdAt called for non-empty coordinate: " + coordinate + " with id: " + id);
+            throw new IllegalStateException("placeIdAt called for non-empty coordinate '" + coordinate + "' with id " + "'" + id);
         }
         grid[coordinate.gridIndex(xSize)] = id;
     }
 
-    public void moveId(long id, WaTorCoordinate oldCoordinate, WaTorCoordinate newCoordinate) {
+    void moveId(long id, WaTorCoordinate oldCoordinate, WaTorCoordinate newCoordinate) {
         if (isEmpty(oldCoordinate)) {
-            throw new IllegalStateException("moveId called for empty coordinate: " + oldCoordinate + " with id: " + id);
+            throw new IllegalStateException("moveId called for empty oldCoordinate '" + oldCoordinate + "' with id " + "'" + id);
         }
         if (!isEmpty(newCoordinate)) {
-            throw new IllegalStateException("moveId called for non-empty coordinate: " + newCoordinate + " with id: " + id);
+            throw new IllegalStateException("moveId called for non-empty newCoordinate '" + newCoordinate + "' with id " + "'" + id);
         }
         removeIdAt(oldCoordinate);
         placeIdAt(id, newCoordinate);
     }
 
-    public void removeIdAt(WaTorCoordinate coordinate) {
+    void removeIdAt(WaTorCoordinate coordinate) {
         if (isEmpty(coordinate)) {
-            throw new IllegalStateException("removeIdAt called for empty coordinate: " + coordinate);
+            throw new IllegalStateException("removeIdAt called for empty coordinate '" + coordinate + "'");
         }
         grid[coordinate.gridIndex(xSize)] = null;
     }
