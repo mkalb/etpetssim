8f7920ebe0209dd9b789c8a8830d1bf2ff91c3bc
MathiasKalb <mkalb@mkalb.de>
2025-06-22 17:31:09 +0200

    Fix module-info.java and add logging and core

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -2,6 +2,8 @@
 module de.mkalb.etpetssim {
     requires org.jspecify;
     requires javafx.controls;
+    requires java.logging;
     exports de.mkalb.etpetssim;
+    exports de.mkalb.etpetssim.core;
     exports de.mkalb.etpetssim.wator;
 }
