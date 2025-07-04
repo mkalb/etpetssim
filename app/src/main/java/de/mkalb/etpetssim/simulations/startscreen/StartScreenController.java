75587276bf22410e03fa04fe6b0fc8ee4f3eeab1
MathiasKalb <mkalb@mkalb.de>
2025-07-04 18:06:38 +0200

    Add title to StartScreenController

--- app/src/main/java/de/mkalb/etpetssim/simulations/startscreen/StartScreenController.java
+++ app/src/main/java/de/mkalb/etpetssim/simulations/startscreen/StartScreenController.java
@@ -1,8 +1,11 @@
 package de.mkalb.etpetssim.simulations.startscreen;
 
+import de.mkalb.etpetssim.core.AppLocalization;
 import de.mkalb.etpetssim.simulations.SimulationController;
 import de.mkalb.etpetssim.simulations.SimulationType;
+import de.mkalb.etpetssim.ui.FXComponentBuilder;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
@@ -32,17 +35,20 @@ public final class StartScreenController implements SimulationController {
     public Region buildViewRegion() {
         VBox root = new VBox();
 
+        Label titleLabel = FXComponentBuilder.createLabel(AppLocalization.getText(SimulationType.labelResourceKey()), "startscreen-title-label");
+        root.getChildren().add(titleLabel);
+
         for (SimulationType type : SimulationType.values()) {
             if (!type.isShownOnStartScreen()) {
                 continue;
             }
 
             Button button = new Button(type.title());
-            type.subtitle().ifPresent(subtitle -> button.setTooltip(new Tooltip(subtitle)));
-            button.setDisable(!type.isImplemented());
-
             if (type.isImplemented()) {
-                button.setOnAction(event -> stageUpdater.accept(stage, type));
+                type.subtitle().ifPresent(subtitle -> button.setTooltip(new Tooltip(subtitle)));
+                button.setOnAction(_ -> stageUpdater.accept(stage, type));
+            } else {
+                button.setDisable(true);
             }
 
             root.getChildren().add(button);
