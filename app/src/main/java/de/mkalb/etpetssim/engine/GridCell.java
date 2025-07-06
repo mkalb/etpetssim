cb9bc42a448cf8889fecf17b8256d4690ece829b
MathiasKalb <mkalb@mkalb.de>
2025-07-06 08:51:26 +0200

    Add JavaDoc

--- app/src/main/java/de/mkalb/etpetssim/engine/GridCell.java
+++ app/src/main/java/de/mkalb/etpetssim/engine/GridCell.java
@@ -2,8 +2,25 @@ package de.mkalb.etpetssim.engine;
 
 import java.util.*;
 
+/**
+ * A record representing a cell in a grid model.
+ *
+ * @param <T> The type of the value stored in the cell.
+ * @param coordinate The coordinate of the cell in the grid.
+ * @param value The value associated with the cell.
+ *
+ * @see de.mkalb.etpetssim.engine.GridModel
+ */
 public record GridCell<T>(GridCoordinate coordinate, T value) {
 
+    /**
+     * Constructs a new GridCell instance.
+     * Ensures that neither the coordinate nor the value is null.
+     *
+     * @param coordinate The coordinate of the cell in the grid.
+     * @param value The value associated with the cell.
+     * @throws NullPointerException if either the coordinate or value is null.
+     */
     public GridCell {
         Objects.requireNonNull(coordinate);
         Objects.requireNonNull(value);
