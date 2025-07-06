54727cc39ab5d1e76fcb47c78a8c83796bde0d55
MathiasKalb <mkalb@mkalb.de>
2025-07-06 15:46:04 +0200

    Add method "descriptorId" to GridEntity

--- app/src/main/java/de/mkalb/etpetssim/engine/model/GridEntity.java
+++ app/src/main/java/de/mkalb/etpetssim/engine/model/GridEntity.java
@@ -22,4 +22,15 @@ public interface GridEntity {
         return entity instanceof ConstantGridEntity;
     }
 
+    /**
+     * Retrieves the unique identifier of the descriptor associated with this grid entity.
+     * <p>
+     * The descriptor ID is used to look up the {@link GridEntityDescriptor} in the registry,
+     * providing metadata and localization information for the entity.
+     * </p>
+     *
+     * @return the unique descriptor ID as a {@link String}
+     */
+    String descriptorId();
+
 }
