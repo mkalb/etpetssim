795a04c529a1138d8909e5f098a9aeb84adbd430
MathiasKalb <mkalb@mkalb.de>
2025-07-11 20:10:28 +0200

    Add a pattern to teh model

--- app/src/main/java/de/mkalb/etpetssim/simulations/simulationlab/SimulationLabController.java
+++ app/src/main/java/de/mkalb/etpetssim/simulations/simulationlab/SimulationLabController.java
@@ -36,11 +36,20 @@ public final class SimulationLabController implements SimulationController {
         cellEdgeLength = (cellShape == CellShape.HEXAGON) ? hexagonEdgeLength : (2 * hexagonEdgeLength);
 
         structure = new GridStructure(new GridTopology(cellShape, GridEdgeBehavior.WRAP_X_WRAP_Y),
-                GridSize.SMALL_SQUARE);
+                new GridSize(30, 30));
 
         model = new SparseGridModel<>(structure, SimulationLabEntity.NORMAL);
         GridInitializers.placeRandomCounted(3, () -> SimulationLabEntity.HIGHLIGHTED, new Random())
                         .initialize(model);
+
+        // Place a symmetric small cross pattern of highlighted entities
+        GridEntityUtils.placePatternAt(new GridCoordinate(5, 5), model,
+                GridPattern.of(SimulationLabEntity.HIGHLIGHTED, List.of(
+                        new GridOffset(0, 1),
+                        new GridOffset(1, 0),
+                        new GridOffset(2, 1),
+                        new GridOffset(1, 2)
+                )));
     }
 
     @Override
