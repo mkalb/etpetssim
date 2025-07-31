9aeccc854594b911f8dfb7172d3b37d12f2209ab
MathiasKalb <mkalb@mkalb.de>
2025-07-31 19:31:39 +0200

    Update termination condition logic to include fish population thresholds

--- app/src/main/java/de/mkalb/etpetssim/simulations/wator/model/WatorTerminationCondition.java
+++ app/src/main/java/de/mkalb/etpetssim/simulations/wator/model/WatorTerminationCondition.java
@@ -5,9 +5,13 @@ import de.mkalb.etpetssim.engine.model.SimulationTerminationCondition;
 
 public final class WatorTerminationCondition implements SimulationTerminationCondition<WatorEntity, WatorStatistics> {
 
+    private static final double FISH_MAX_PERCENTAGE = 0.9d;
+
     @Override
     public boolean isFinished(GridModel<WatorEntity> model, long step, WatorStatistics statistics) {
-        return statistics.getSharkCells() == 0;
+        return (statistics.getSharkCells() == 0)
+                && ((statistics.getFishCells() == 0)
+                || (statistics.getFishCells() > (statistics.getTotalCells() * FISH_MAX_PERCENTAGE)));
     }
 
 }
