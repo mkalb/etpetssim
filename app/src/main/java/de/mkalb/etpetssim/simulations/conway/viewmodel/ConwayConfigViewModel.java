23e829d49fe18d9c4c50d43c14735dba7a824748
MathiasKalb <mkalb@mkalb.de>
2025-08-21 21:59:59 +0200

    Add support for custom survival and birth rules in Conway configuration

--- app/src/main/java/de/mkalb/etpetssim/simulations/conway/viewmodel/ConwayConfigViewModel.java
+++ app/src/main/java/de/mkalb/etpetssim/simulations/conway/viewmodel/ConwayConfigViewModel.java
@@ -9,7 +9,9 @@ import de.mkalb.etpetssim.simulations.model.SimulationState;
 import de.mkalb.etpetssim.simulations.viewmodel.AbstractConfigViewModel;
 import de.mkalb.etpetssim.ui.InputDoubleProperty;
 import de.mkalb.etpetssim.ui.InputEnumProperty;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 
 import java.util.*;
 
@@ -32,6 +34,7 @@ public final class ConwayConfigViewModel
             1,
             48);
     private static final NeighborhoodMode NEIGHBORHOOD_MODE_INITIAL = NeighborhoodMode.EDGES_AND_VERTICES;
+    private static final ConwayRules DEFAULT_RULES = ConwayRules.of(Set.of(2, 3), Set.of(3));
 
     private static final double ALIVE_PERCENT_INITIAL = 0.15d;
     private static final double ALIVE_PERCENT_MAX = 1.0d;
@@ -44,13 +47,34 @@ public final class ConwayConfigViewModel
     private final InputEnumProperty<NeighborhoodMode> neighborhoodMode =
             InputEnumProperty.of(NEIGHBORHOOD_MODE_INITIAL, NeighborhoodMode.class,
                     e -> AppLocalization.getOptionalText(e.resourceKey()).orElse(e.toString()));
+    private final List<BooleanProperty> surviveProperties = new ArrayList<>();
+    private final List<BooleanProperty> bornProperties = new ArrayList<>();
 
     public ConwayConfigViewModel(ReadOnlyObjectProperty<SimulationState> simulationState) {
         super(simulationState, STRUCTURE_SETTINGS);
+
+        for (int i = ConwayRules.MIN_NEIGHBOR_COUNT; i <= ConwayRules.MAX_NEIGHBOR_COUNT; i++) {
+            surviveProperties.add(new SimpleBooleanProperty(DEFAULT_RULES.surviveCounts().contains(i)));
+            bornProperties.add(new SimpleBooleanProperty(DEFAULT_RULES.birthCounts().contains(i)));
+        }
+
+        // HEXAGON: 23/34
+        // SQUARE: 23/3
+        // TRIANGLE: 45/456
     }
 
     @Override
     public ConwayConfig getConfig() {
+        SortedSet<Integer> surviveCounts = new TreeSet<>();
+        SortedSet<Integer> birthCounts = new TreeSet<>();
+        for (int i = ConwayRules.MIN_NEIGHBOR_COUNT; i <= ConwayRules.MAX_NEIGHBOR_COUNT; i++) {
+            if (surviveProperties.get(i - ConwayRules.MIN_NEIGHBOR_COUNT).get()) {
+                surviveCounts.add(i);
+            }
+            if (bornProperties.get(i - ConwayRules.MIN_NEIGHBOR_COUNT).get()) {
+                birthCounts.add(i);
+            }
+        }
         return new ConwayConfig(
                 cellShapeProperty().property().getValue(),
                 gridEdgeBehaviorProperty().property().getValue(),
@@ -59,7 +83,7 @@ public final class ConwayConfigViewModel
                 cellEdgeLengthProperty().property().getValue(),
                 alivePercent.getValue(),
                 neighborhoodMode.getValue(),
-                ConwayRules.of("23/3")
+                new ConwayRules(surviveCounts, birthCounts)
         );
     }
 
@@ -69,6 +93,14 @@ public final class ConwayConfigViewModel
 
     public InputEnumProperty<NeighborhoodMode> neighborhoodModeProperty() {
         return neighborhoodMode;
+    }
+
+    public List<BooleanProperty> getSurviveProperties() {
+        return surviveProperties;
+    }
+
+    public List<BooleanProperty> getBornProperties() {
+        return bornProperties;
     }
 
 }
