d0cabe47a3b052cc68451abdc19011598834d1f3
MathiasKalb <mkalb@mkalb.de>
2025-09-07 20:51:37 +0200

    Add coordinate and age labels to WatorObservationView and update their values based on selected grid cell

--- app/src/main/java/de/mkalb/etpetssim/simulations/wator/view/WatorObservationView.java
+++ app/src/main/java/de/mkalb/etpetssim/simulations/wator/view/WatorObservationView.java
@@ -2,8 +2,10 @@ package de.mkalb.etpetssim.simulations.wator.view;
 
 import de.mkalb.etpetssim.core.AppLocalization;
 import de.mkalb.etpetssim.core.AppLocalizationKeys;
+import de.mkalb.etpetssim.engine.model.GridCell;
 import de.mkalb.etpetssim.simulations.view.AbstractObservationView;
 import de.mkalb.etpetssim.simulations.viewmodel.DefaultObservationViewModel;
+import de.mkalb.etpetssim.simulations.wator.model.WatorCreature;
 import de.mkalb.etpetssim.simulations.wator.model.WatorEntity;
 import de.mkalb.etpetssim.simulations.wator.model.WatorStatistics;
 import javafx.scene.control.Label;
@@ -23,6 +25,8 @@ public final class WatorObservationView
     static final String WATOR_OBSERVATION_MIN_SHARK_CELLS = "wator.observation.cells.minshark";
     static final String WATOR_OBSERVATION_FISH_CELLS = "wator.observation.cells.fish";
     static final String WATOR_OBSERVATION_SHARK_CELLS = "wator.observation.cells.shark";
+    static final String WATOR_OBSERVATION_COORDINATE = "wator.observation.coordinate";
+    static final String WATOR_OBSERVATION_AGE = "wator.observation.age";
 
     private final Label stepCountLabel = new Label();
     private final Label totalCellsLabel = new Label();
@@ -32,11 +36,30 @@ public final class WatorObservationView
     private final Label minSharkCellsLabel = new Label();
     private final Label fishCellsLabel = new Label();
     private final Label sharkCellsLabel = new Label();
+    private final Label coordinateLabel = new Label();
+    private final Label ageLabel = new Label();
 
     private @Nullable NumberFormat intFormat;
 
     public WatorObservationView(DefaultObservationViewModel<WatorEntity, WatorStatistics> viewModel) {
         super(viewModel);
+
+        viewModel.selectedGridCellProperty().addListener((_, _, newCell) ->
+                updateSelectedGridCell(newCell));
+    }
+
+    private void updateSelectedGridCell(@Nullable GridCell<WatorEntity> gridCell) {
+        Optional<WatorStatistics> statistics = viewModel.getStatistics();
+        if (statistics.isPresent()
+                && (gridCell != null)
+                && (intFormat != null)
+                && (gridCell.entity() instanceof WatorCreature creature)) {
+            coordinateLabel.setText(gridCell.coordinate().toDisplayString());
+            ageLabel.setText(intFormat.format(creature.ageAtStepCount(statistics.get().getStepCount())));
+        } else {
+            coordinateLabel.setText("");
+            ageLabel.setText("");
+        }
     }
 
     @Override
@@ -51,7 +74,9 @@ public final class WatorObservationView
                 WATOR_OBSERVATION_MIN_FISH_CELLS,
                 WATOR_OBSERVATION_MIN_SHARK_CELLS,
                 WATOR_OBSERVATION_FISH_CELLS,
-                WATOR_OBSERVATION_SHARK_CELLS
+                WATOR_OBSERVATION_SHARK_CELLS,
+                WATOR_OBSERVATION_COORDINATE,
+                WATOR_OBSERVATION_AGE
         };
         Label[] valueLabels = {
                 stepCountLabel,
@@ -61,7 +86,9 @@ public final class WatorObservationView
                 minFishCellsLabel,
                 minSharkCellsLabel,
                 fishCellsLabel,
-                sharkCellsLabel
+                sharkCellsLabel,
+                coordinateLabel,
+                ageLabel
         };
 
         intFormat = NumberFormat.getIntegerInstance(AppLocalization.locale());
@@ -92,7 +119,6 @@ public final class WatorObservationView
             minSharkCellsLabel.setText(valueUnknown);
             fishCellsLabel.setText(valueUnknown);
             sharkCellsLabel.setText(valueUnknown);
-
         }
     }
 
