f1990e1206b043e52c0442c007b3d6f9791f3aa8
MathiasKalb <mkalb@mkalb.de>
2025-10-03 12:29:13 +0200

    Update JUnit version to 5.14.0 in build configuration and license file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ application {
 testing {
     suites {
         val test by getting(JvmTestSuite::class) {
-            useJUnitJupiter("5.13.4")
+            useJUnitJupiter("5.14.0")
             targets {
                 all {
                     testTask.configure {
