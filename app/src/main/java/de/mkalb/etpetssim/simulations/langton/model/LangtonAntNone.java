bc13ea34a30d4ec28f535a87dec8b64803ed58a0
MathiasKalb <mkalb@mkalb.de>
2025-11-09 21:45:39 +0100

    Simplified LangtonAntNone

--- app/src/main/java/de/mkalb/etpetssim/simulations/langton/model/LangtonAntNone.java
+++ app/src/main/java/de/mkalb/etpetssim/simulations/langton/model/LangtonAntNone.java
@@ -1,11 +1,8 @@
 package de.mkalb.etpetssim.simulations.langton.model;
 
-import de.mkalb.etpetssim.engine.model.GridEntityDescribable;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import org.jspecify.annotations.Nullable;
+import de.mkalb.etpetssim.engine.model.ConstantGridEntity;
 
-public enum LangtonAntNone implements LangtonAntEntity, GridEntityDescribable {
+public enum LangtonAntNone implements LangtonAntEntity, ConstantGridEntity {
 
     NONE;
 
@@ -17,55 +14,6 @@ public enum LangtonAntNone implements LangtonAntEntity, GridEntityDescribable {
     @Override
     public String descriptorId() {
         return LangtonAntEntity.DESCRIPTOR_ID_ANT_NONE;
-    }
-
-    @Override
-    public boolean visible() {
-        return false;
-    }
-
-    @Override
-    public String shortKey() {
-        return "";
-    }
-
-    @Override
-    public String longKey() {
-        return "";
-    }
-
-    @Override
-    public String descriptionKey() {
-        return "";
-    }
-
-    @Override
-    public @Nullable String emojiKey() {
-        return null;
-    }
-
-    @Override
-    public @Nullable Paint color() {
-        return null;
-    }
-
-    @Override
-    public @Nullable Color borderColor() {
-        return null;
-    }
-
-    @Override
-    public int renderPriority() {
-        return Integer.MAX_VALUE;
-    }
-
-    @Override
-    public String toDisplayString() {
-        return "TODO"; // TODO implement
-    }
-
-    public String toString() {
-        return "TODO"; // TODO implement
     }
 
 }
