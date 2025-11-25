58873e39e7106362ea009d1ecd4fd1992a347f30
MathiasKalb <mkalb@mkalb.de>
2025-11-25 22:32:28 +0100

    Add configuration options for sugar and agent properties in SugarConfigView

--- app/src/main/java/de/mkalb/etpetssim/simulations/sugar/view/SugarConfigView.java
+++ app/src/main/java/de/mkalb/etpetssim/simulations/sugar/view/SugarConfigView.java
@@ -13,6 +13,23 @@ import javafx.scene.layout.Region;
 public final class SugarConfigView
         extends AbstractConfigView<SugarConfig, SugarConfigViewModel> {
 
+    // Initialization
+    static final String SUGAR_CONFIG_SUGAR_PERCENT = "sugar.config.sugarpercent";
+    static final String SUGAR_CONFIG_SUGAR_PERCENT_TOOLTIP = "sugar.config.sugarpercent.tooltip";
+    static final String SUGAR_CONFIG_AGENT_PERCENT = "sugar.config.agentpercent";
+    static final String SUGAR_CONFIG_AGENT_PERCENT_TOOLTIP = "sugar.config.agentpercent.tooltip";
+    static final String SUGAR_CONFIG_MAX_SUGAR_AMOUNT = "sugar.config.maxsugaramount";
+    static final String SUGAR_CONFIG_MAX_SUGAR_AMOUNT_TOOLTIP = "sugar.config.maxsugaramount.tooltip";
+    static final String SUGAR_CONFIG_AGENT_INITIAL_ENERGY = "sugar.config.agentinitialenergy";
+    static final String SUGAR_CONFIG_AGENT_INITIAL_ENERGY_TOOLTIP = "sugar.config.agentinitialenergy.tooltip";
+    // Rules
+    static final String SUGAR_CONFIG_SUGAR_REGENERATION_RATE = "sugar.config.sugarregenerationrate";
+    static final String SUGAR_CONFIG_SUGAR_REGENERATION_RATE_TOOLTIP = "sugar.config.sugarregenerationrate.tooltip";
+    static final String SUGAR_CONFIG_AGENT_METABOLISM_RATE = "sugar.config.agentmetabolismrate";
+    static final String SUGAR_CONFIG_AGENT_METABOLISM_RATE_TOOLTIP = "sugar.config.agentmetabolismrate.tooltip";
+    static final String SUGAR_CONFIG_AGENT_VISION_RANGE = "sugar.config.agentvisionrange";
+    static final String SUGAR_CONFIG_AGENT_VISION_RANGE_TOOLTIP = "sugar.config.agentvisionrange.tooltip";
+
     public SugarConfigView(SugarConfigViewModel viewModel) {
         super(viewModel);
     }
@@ -32,13 +49,58 @@ public final class SugarConfigView
                 AppLocalization.getText(AppLocalizationKeys.CONFIG_SEED_CLEAR_TOOLTIP),
                 FXStyleClasses.CONFIG_TEXTBOX
         );
+        var sugarPercentControl = FXComponentFactory.createLabeledPercentSlider(
+                viewModel.sugarPercentProperty(),
+                AppLocalization.getText(SUGAR_CONFIG_SUGAR_PERCENT),
+                AppLocalization.getText(SUGAR_CONFIG_SUGAR_PERCENT_TOOLTIP),
+                FXStyleClasses.CONFIG_SLIDER
+        );
+        var agentPercentControl = FXComponentFactory.createLabeledPercentSlider(
+                viewModel.agentPercentProperty(),
+                AppLocalization.getText(SUGAR_CONFIG_AGENT_PERCENT),
+                AppLocalization.getText(SUGAR_CONFIG_AGENT_PERCENT_TOOLTIP),
+                FXStyleClasses.CONFIG_SLIDER
+        );
+        var maxSugarAmountControl = FXComponentFactory.createLabeledIntSpinner(
+                viewModel.maxSugarAmountProperty(),
+                AppLocalization.getText(SUGAR_CONFIG_MAX_SUGAR_AMOUNT),
+                AppLocalization.getText(SUGAR_CONFIG_MAX_SUGAR_AMOUNT_TOOLTIP),
+                FXStyleClasses.CONFIG_SLIDER
+        );
+        var agentInitialEnergyControl = FXComponentFactory.createLabeledIntSpinner(
+                viewModel.agentInitialEnergyProperty(),
+                AppLocalization.getText(SUGAR_CONFIG_AGENT_INITIAL_ENERGY),
+                AppLocalization.getText(SUGAR_CONFIG_AGENT_INITIAL_ENERGY_TOOLTIP),
+                FXStyleClasses.CONFIG_SLIDER
+        );
 
         TitledPane initPane = createConfigTitledPane(AppLocalization.getText(AppLocalizationKeys.CONFIG_TITLE_INITIALIZATION),
-                true, seedControl);
+                true,
+                seedControl, sugarPercentControl, agentPercentControl, maxSugarAmountControl, agentInitialEnergyControl);
 
         // --- Rules Group ---
+        var sugarRegenerationRateControl = FXComponentFactory.createLabeledIntSpinner(
+                viewModel.sugarRegenerationRateProperty(),
+                AppLocalization.getText(SUGAR_CONFIG_SUGAR_REGENERATION_RATE),
+                AppLocalization.getText(SUGAR_CONFIG_SUGAR_REGENERATION_RATE_TOOLTIP),
+                FXStyleClasses.CONFIG_SLIDER
+        );
+        var agentMetabolismRateControl = FXComponentFactory.createLabeledIntSpinner(
+                viewModel.agentMetabolismRateProperty(),
+                AppLocalization.getText(SUGAR_CONFIG_AGENT_METABOLISM_RATE),
+                AppLocalization.getText(SUGAR_CONFIG_AGENT_METABOLISM_RATE_TOOLTIP),
+                FXStyleClasses.CONFIG_SLIDER
+        );
+        var agentVisionRangeControl = FXComponentFactory.createLabeledIntSpinner(
+                viewModel.agentVisionRangeProperty(),
+                AppLocalization.getText(SUGAR_CONFIG_AGENT_VISION_RANGE),
+                AppLocalization.getText(SUGAR_CONFIG_AGENT_VISION_RANGE_TOOLTIP),
+                FXStyleClasses.CONFIG_SLIDER
+        );
+
         TitledPane rulesPane = createConfigTitledPane(AppLocalization.getText(AppLocalizationKeys.CONFIG_TITLE_RULES),
-                true);
+                true,
+                sugarRegenerationRateControl, agentMetabolismRateControl, agentVisionRangeControl);
 
         return createConfigMainBox(structurePane, layoutPane, initPane, rulesPane);
     }
--- app/src/main/java/de/mkalb/etpetssim/simulations/sugar/viewmodel/SugarConfigViewModel.java
+++ app/src/main/java/de/mkalb/etpetssim/simulations/sugar/viewmodel/SugarConfigViewModel.java
@@ -8,6 +8,8 @@ import de.mkalb.etpetssim.simulations.core.model.CellDisplayMode;
 import de.mkalb.etpetssim.simulations.core.model.SimulationState;
 import de.mkalb.etpetssim.simulations.core.viewmodel.AbstractConfigViewModel;
 import de.mkalb.etpetssim.simulations.sugar.model.SugarConfig;
+import de.mkalb.etpetssim.ui.InputDoubleProperty;
+import de.mkalb.etpetssim.ui.InputIntegerProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
 
 import java.util.*;
@@ -23,15 +25,15 @@ public final class SugarConfigViewModel
                     GridEdgeBehavior.WRAP_XY,
                     GridEdgeBehavior.BLOCK_X_WRAP_Y,
                     GridEdgeBehavior.WRAP_X_BLOCK_Y),
-            50,
+            100,
             10,
             1_000,
             GridTopology.MAX_REQUIRED_WIDTH_MULTIPLE,
-            50,
+            100,
             10,
             1_000,
             GridTopology.MAX_REQUIRED_HEIGHT_MULTIPLE,
-            12,
+            6,
             4,
             32,
             CellDisplayMode.SHAPE,
@@ -39,11 +41,70 @@ public final class SugarConfigViewModel
             ""
     );
     // Initialization
-    private static final NeighborhoodMode NEIGHBORHOOD_MODE_INITIAL = NeighborhoodMode.EDGES_ONLY;
+    private static final double SUGAR_PERCENT_INITIAL = 0.05d;
+    private static final double SUGAR_PERCENT_MIN = 0.0d;
+    private static final double SUGAR_PERCENT_MAX = 1.0d;
+    private static final double AGENT_PERCENT_INITIAL = 0.05d;
+    private static final double AGENT_PERCENT_MIN = 0.0d;
+    private static final double AGENT_PERCENT_MAX = 1.0d;
+    private static final int MAX_SUGAR_AMOUNT_INITIAL = 6;
+    private static final int MAX_SUGAR_AMOUNT_MIN = 1;
+    private static final int MAX_SUGAR_AMOUNT_MAX = 20;
+    private static final int MAX_SUGAR_AMOUNT_STEP = 1;
+    private static final int AGENT_INITIAL_ENERGY_INITIAL = 5;
+    private static final int AGENT_INITIAL_ENERGY_MIN = 1;
+    private static final int AGENT_INITIAL_ENERGY_MAX = 20;
+    private static final int AGENT_INITIAL_ENERGY_STEP = 1;
     // Rules
+    private static final int SUGAR_REGENERATION_RATE_INITIAL = 1;
+    private static final int SUGAR_REGENERATION_RATE_MIN = 1;
+    private static final int SUGAR_REGENERATION_RATE_MAX = 10;
+    private static final int SUGAR_REGENERATION_RATE_STEP = 1;
+    private static final int AGENT_METABOLISM_RATE_INITIAL = 1;
+    private static final int AGENT_METABOLISM_RATE_MIN = 1;
+    private static final int AGENT_METABOLISM_RATE_MAX = 10;
+    private static final int AGENT_METABOLISM_RATE_STEP = 1;
+    private static final int AGENT_VISION_RANGE_INITIAL = 4;
+    private static final int AGENT_VISION_RANGE_MIN = 1;
+    private static final int AGENT_VISION_RANGE_MAX = 10;
+    private static final int AGENT_VISION_RANGE_STEP = 1;
+    private static final NeighborhoodMode NEIGHBORHOOD_MODE_INITIAL = NeighborhoodMode.EDGES_ONLY;
 
     // Initialization
+    private final InputDoubleProperty sugarPercent = InputDoubleProperty.of(
+            SUGAR_PERCENT_INITIAL,
+            SUGAR_PERCENT_MIN,
+            SUGAR_PERCENT_MAX);
+    private final InputDoubleProperty agentPercent = InputDoubleProperty.of(
+            AGENT_PERCENT_INITIAL,
+            AGENT_PERCENT_MIN,
+            AGENT_PERCENT_MAX);
+    private final InputIntegerProperty maxSugarAmount = InputIntegerProperty.of(
+            MAX_SUGAR_AMOUNT_INITIAL,
+            MAX_SUGAR_AMOUNT_MIN,
+            MAX_SUGAR_AMOUNT_MAX,
+            MAX_SUGAR_AMOUNT_STEP);
+    private final InputIntegerProperty agentInitialEnergy = InputIntegerProperty.of(
+            AGENT_INITIAL_ENERGY_INITIAL,
+            AGENT_INITIAL_ENERGY_MIN,
+            AGENT_INITIAL_ENERGY_MAX,
+            AGENT_INITIAL_ENERGY_STEP);
     // Rules
+    private final InputIntegerProperty sugarRegenerationRate = InputIntegerProperty.of(
+            SUGAR_REGENERATION_RATE_INITIAL,
+            SUGAR_REGENERATION_RATE_MIN,
+            SUGAR_REGENERATION_RATE_MAX,
+            SUGAR_REGENERATION_RATE_STEP);
+    private final InputIntegerProperty agentMetabolismRate = InputIntegerProperty.of(
+            AGENT_METABOLISM_RATE_INITIAL,
+            AGENT_METABOLISM_RATE_MIN,
+            AGENT_METABOLISM_RATE_MAX,
+            AGENT_METABOLISM_RATE_STEP);
+    private final InputIntegerProperty agentVisionRange = InputIntegerProperty.of(
+            AGENT_VISION_RANGE_INITIAL,
+            AGENT_VISION_RANGE_MIN,
+            AGENT_VISION_RANGE_MAX,
+            AGENT_VISION_RANGE_STEP);
 
     public SugarConfigViewModel(ReadOnlyObjectProperty<SimulationState> simulationState) {
         super(simulationState, COMMON_SETTINGS);
@@ -59,15 +120,43 @@ public final class SugarConfigViewModel
                 cellEdgeLengthProperty().property().getValue(),
                 cellDisplayModeProperty().property().getValue(),
                 seedProperty().computeSeedAndUpdateLabel(),
-                0.1d,
-                0.05d,
-                6,
-                5,
-                1,
-                1,
-                4,
+                sugarPercent.getValue(),
+                agentPercent.getValue(),
+                maxSugarAmount.getValue(),
+                agentInitialEnergy.getValue(),
+                sugarRegenerationRate.getValue(),
+                agentMetabolismRate.getValue(),
+                agentVisionRange.getValue(),
                 NEIGHBORHOOD_MODE_INITIAL
         );
+    }
+
+    public InputDoubleProperty sugarPercentProperty() {
+        return sugarPercent;
+    }
+
+    public InputDoubleProperty agentPercentProperty() {
+        return agentPercent;
+    }
+
+    public InputIntegerProperty maxSugarAmountProperty() {
+        return maxSugarAmount;
+    }
+
+    public InputIntegerProperty agentInitialEnergyProperty() {
+        return agentInitialEnergy;
+    }
+
+    public InputIntegerProperty sugarRegenerationRateProperty() {
+        return sugarRegenerationRate;
+    }
+
+    public InputIntegerProperty agentMetabolismRateProperty() {
+        return agentMetabolismRate;
+    }
+
+    public InputIntegerProperty agentVisionRangeProperty() {
+        return agentVisionRange;
     }
 
 }
