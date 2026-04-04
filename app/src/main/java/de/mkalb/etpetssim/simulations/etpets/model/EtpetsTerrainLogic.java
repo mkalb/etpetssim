dee7ca2a8d85b4dd39e719dcc12da32f675ad6ad
Mathias Kalb <mkalb@mkalb.de>
2026-04-04 22:05:02 +0200

    Fix bug at EtpetsTerrainLogic

--- app/src/main/java/de/mkalb/etpetssim/simulations/etpets/model/EtpetsTerrainLogic.java
+++ app/src/main/java/de/mkalb/etpetssim/simulations/etpets/model/EtpetsTerrainLogic.java
@@ -1,6 +1,8 @@
 package de.mkalb.etpetssim.simulations.etpets.model;
 
+import de.mkalb.etpetssim.engine.model.GridCell;
 import de.mkalb.etpetssim.simulations.etpets.model.entity.EtpetsTerrainConstant;
+import de.mkalb.etpetssim.simulations.etpets.model.entity.EtpetsTerrainEntity;
 import de.mkalb.etpetssim.simulations.etpets.model.entity.EtpetsTerrainTrail;
 
 public final class EtpetsTerrainLogic {
@@ -11,14 +13,17 @@ public final class EtpetsTerrainLogic {
     }
 
     public static void apply(EtpetsGridModel gridModel, EtpetsConfig config, int stepIndex, EtpetsStatistics statistics) {
-        gridModel.terrainModel().nonDefaultCells().forEach(cell -> {
+        var nonDefaultCells = gridModel.terrainModel()
+                                       .nonDefaultCells()
+                                       .toList();
+        for (GridCell<EtpetsTerrainEntity> cell : nonDefaultCells) {
             if (cell.entity() instanceof EtpetsTerrainTrail trail) {
                 trail.decay(TRAIL_DECAY_PER_STEP);
                 if (trail.isDepleted()) {
                     gridModel.terrainModel().setEntity(cell.coordinate(), EtpetsTerrainConstant.GROUND);
                 }
             }
-        });
+        }
     }
 
 }
