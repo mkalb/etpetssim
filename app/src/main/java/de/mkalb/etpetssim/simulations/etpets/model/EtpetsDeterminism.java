6bf4d6d89f5867e9e722d86e809a48c72f0e61a1
Mathias Kalb <mkalb@mkalb.de>
2026-04-25 21:02:01 +0200

    Remove unused comparePetsForReproduction method in EtpetsDeterminism

--- app/src/main/java/de/mkalb/etpetssim/simulations/etpets/model/EtpetsDeterminism.java
+++ app/src/main/java/de/mkalb/etpetssim/simulations/etpets/model/EtpetsDeterminism.java
@@ -1,7 +1,6 @@
 package de.mkalb.etpetssim.simulations.etpets.model;
 
 import de.mkalb.etpetssim.engine.GridCoordinate;
-import de.mkalb.etpetssim.simulations.etpets.model.entity.Pet;
 
 public final class EtpetsDeterminism {
 
@@ -14,29 +13,6 @@ public final class EtpetsDeterminism {
             return xCompare;
         }
         return Integer.compare(left.y(), right.y());
-    }
-
-    /**
-     * Compares two pet/coordinate pairs for reproduction partner selection priority.
-     * <p>
-     * Order: highest genomeQualityScore (desc), highest currentEnergy (desc),
-     * lowest petId (asc), then coordinate order (asc).
-     */
-    public static int comparePetsForReproduction(Pet a, GridCoordinate aCoord,
-                                                 Pet b, GridCoordinate bCoord) {
-        int qCmp = Double.compare(b.traits().genomeQualityScore(), a.traits().genomeQualityScore());
-        if (qCmp != 0) {
-            return qCmp;
-        }
-        int eCmp = Integer.compare(b.currentEnergy(), a.currentEnergy());
-        if (eCmp != 0) {
-            return eCmp;
-        }
-        int idCmp = Long.compare(a.petId(), b.petId());
-        if (idCmp != 0) {
-            return idCmp;
-        }
-        return compareCoordinates(aCoord, bCoord);
     }
 
 }
