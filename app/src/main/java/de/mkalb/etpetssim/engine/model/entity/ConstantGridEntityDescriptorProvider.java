5cf522620b7c8d2ee35fd8d84aa8e4a2b64d1744
Mathias Kalb <mkalb@mkalb.de>
2026-05-09 21:51:51 +0200

    Interface ConstantGridEntityDescriptorProvider vereinfacht

--- app/src/main/java/de/mkalb/etpetssim/engine/model/entity/ConstantGridEntityDescriptorProvider.java
+++ app/src/main/java/de/mkalb/etpetssim/engine/model/entity/ConstantGridEntityDescriptorProvider.java
@@ -1,67 +1,20 @@
 package de.mkalb.etpetssim.engine.model.entity;
 
-import javafx.scene.paint.Color;
-import org.jspecify.annotations.Nullable;
-
 /**
- * Convenience interface for constant grid entities that provide descriptor metadata.
- * <p>
- * Implementations provide one {@link GridEntityDescriptorSpec} and reuse the default
- * descriptor accessors from this interface.
- * </p>
+ * Convenience interface for constant grid entities that expose descriptor metadata
+ * through a {@link GridEntityDescriptorSpec}.
  *
  * @see ConstantGridEntity
  * @see GridEntityDescriptorProvider
  * @see GridEntityDescriptorSpec
  */
 @SuppressWarnings("InterfaceMayBeAnnotatedFunctional")
-public interface ConstantGridEntityDescriptorProvider extends ConstantGridEntity, GridEntityDescriptorProvider {
-
-    /**
-     * Returns the immutable descriptor specification for this entity.
-     *
-     * @return the {@link GridEntityDescriptorSpec} containing raw descriptor keys and rendering hints
-     */
-    GridEntityDescriptorSpec descriptorSpec();
+public interface ConstantGridEntityDescriptorProvider
+        extends ConstantGridEntity, SpecBackedGridEntityDescriptorProvider {
 
     @Override
     default String descriptorId() {
-        return descriptorSpec().descriptorId();
-    }
-
-    @Override
-    default boolean visible() {
-        return descriptorSpec().visible();
-    }
-
-    @Override
-    default String shortNameKey() {
-        return descriptorSpec().shortNameKey();
-    }
-
-    @Override
-    default String longNameKey() {
-        return descriptorSpec().longNameKey();
-    }
-
-    @Override
-    default String descriptionKey() {
-        return descriptorSpec().descriptionKey();
-    }
-
-    @Override
-    default @Nullable String emojiKey() {
-        return descriptorSpec().emojiKey();
-    }
-
-    @Override
-    default @Nullable Color color() {
-        return descriptorSpec().color();
-    }
-
-    @Override
-    default @Nullable Color borderColor() {
-        return descriptorSpec().borderColor();
+        return SpecBackedGridEntityDescriptorProvider.super.descriptorId();
     }
 
 }
